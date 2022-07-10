edfaa8ec1ed0b1224ebbd47f1e0d2b2d71e99431
Yourstruggle11 <sensouvik51@gmail.com>
2022-07-11 02:15:26 +0530

    fix: fixed some glitches

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
+
 
 function App() {
   return (
-    <>
-        <h1 className="text-4xl">Hello weather</h1>
-        <h1>Hello weather</h1>
-        <button className='hover:decoration-blue-400'>hello</button>
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
